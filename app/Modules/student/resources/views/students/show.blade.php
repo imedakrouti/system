b7644a1ed61d9f70ed70c4c28d5fa1e4124d5bc7
amr <amr_dotnet@ymail.com>
2020-09-03 01:49:10 +0200

    edit in show student page

--- app/Modules/student/resources/views/students/show.blade.php
+++ app/Modules/student/resources/views/students/show.blade.php
@@ -33,19 +33,22 @@
                     | <a href="{{route('mother.show',$student->mother_id)}}">{{$student->mother->full_name}}</a>  
                 </h2> 
               </div>
+              
             <div class="col-md-12">
                 <a href="{{route('student.create',$student->father_id)}}" class="mb-1 btn btn-success white"><i class="la la-plus"></i> {{ trans('student::local.add_sibling') }}</a>
                 <a href="{{route('students.edit',$student->id)}}" class="mb-1 btn btn-warning white"><i class="la la-edit"></i> {{ trans('student::local.edit') }}</a>
-                <a href="#" class="mb-1 btn btn-info white"><i class="la la-archive"></i> {{ trans('student::local.archive') }}</a>
-                <a href="#" class="mb-1 btn btn-info white"><i class="la la-cc-visa"></i> {{ trans('student::local.payments') }}</a>                
-                <a href="#" class="mb-1 btn btn-info white"><i class="la la-book"></i> {{ trans('student::local.books') }}</a>
-                <a href="#" class="mb-1 btn btn-info white"><i class="la la-tag"></i> {{ trans('student::local.uniform') }}</a>
-                <a href="#" class="mb-1 btn btn-info white"><i class="la la-bed"></i> {{ trans('student::local.absence') }}</a>
-                <a href="#" class="mb-1 btn btn-info white"><i class="la la-file"></i> {{ trans('student::local.parent_requests') }}</a>
-                <a href="#" class="mb-1 btn btn-info white"><i class="la la-file"></i> {{ trans('student::local.permissions') }}</a>
-                <a href="#" class="mb-1 btn btn-info white"><i class="la la-users"></i> {{ trans('student::local.authorizations') }}</a>
-                <a href="#" class="mb-1 btn btn-info white"><i class="la la-bus"></i> {{ trans('student::local.buses') }}</a>                
-                <a href="#" class="mb-1 btn btn-info white"><i class="la la-bus"></i> {{ trans('student::local.health_status') }}</a>                
+                @if ($student->student_type == trans('student::local.student'))                  
+                    <a href="#" class="mb-1 btn btn-info white"><i class="la la-archive"></i> {{ trans('student::local.archive') }}</a>
+                    <a href="#" class="mb-1 btn btn-info white"><i class="la la-cc-visa"></i> {{ trans('student::local.payments') }}</a>                
+                    <a href="#" class="mb-1 btn btn-info white"><i class="la la-book"></i> {{ trans('student::local.books') }}</a>
+                    <a href="#" class="mb-1 btn btn-info white"><i class="la la-tag"></i> {{ trans('student::local.uniform') }}</a>
+                    <a href="#" class="mb-1 btn btn-info white"><i class="la la-bed"></i> {{ trans('student::local.absence') }}</a>
+                    <a href="#" class="mb-1 btn btn-info white"><i class="la la-file"></i> {{ trans('student::local.parent_requests') }}</a>
+                    <a href="#" class="mb-1 btn btn-info white"><i class="la la-file"></i> {{ trans('student::local.permissions') }}</a>
+                    <a href="#" class="mb-1 btn btn-info white"><i class="la la-users"></i> {{ trans('student::local.authorizations') }}</a>
+                    <a href="#" class="mb-1 btn btn-info white"><i class="la la-bus"></i> {{ trans('student::local.buses') }}</a>                
+                    <a href="#" class="mb-1 btn btn-info white"><i class="la la-bus"></i> {{ trans('student::local.health_status') }}</a>                
+                @endif
             </div>
           </div>
         </div>
