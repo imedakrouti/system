bbc36443e325aa0cf3982a258053b4116f7528a7
Abdoahmed111 <eng.abdelrahman.ahmed11@gmail.com>
2020-10-15 07:52:05 +0200

    fixing the form layout of student-statemnt page

--- app/Modules/student/resources/views/students-affairs/students-statements/includes/_filter.blade.php
+++ app/Modules/student/resources/views/students-affairs/students-statements/includes/_filter.blade.php
@@ -1,14 +1,14 @@
 <form action="#" method="get" id="filterForm" target="_blank">
     <div class="row mt-1">
-        <div class="col-md-2 col-xs-12">
-            <select name="year_id" class="form-control select2" id="filter_year_id" required>                
+        <div class="col-lg-2 col-md-5 mb-md-1">
+            <select name="year_id" class="form-control select2" style="width: 100%" id="filter_year_id" required>                
                 @foreach ($years as $year)
                     <option {{currentYear() == $year->id ? 'selected' : ''}} value="{{$year->id}}">{{$year->name}}</option>                                    
                 @endforeach
             </select>
         </div>        
-        <div class="col-md-2 col-xs-12">
-            <select name="division_id[]" class="form-control select2" multiple id="filter_division_id" required>
+        <div class="col-lg-2 col-md-5 mb-md-1">
+            <select name="division_id[]" class="form-control select2" style="width: 100%" multiple id="filter_division_id" required>
                 {{-- <option value="">{{ trans('student::local.divisions') }}</option> --}}
                 @foreach ($divisions as $division)
                     <option value="{{$division->id}}">
@@ -16,8 +16,8 @@
                 @endforeach
             </select>
         </div>
-        <div class="col-md-2 col-xs-12">
-            <select name="grade_id" class="form-control select2" id="filter_grade_id">                
+        <div class="col-lg-2 col-md-5 mb-md-1">
+            <select name="grade_id" class="form-control select2" style="width: 100%" id="filter_grade_id">                
                 @foreach ($grades as $grade)
                     <option value="{{$grade->id}}">
                         {{session('lang') =='ar' ?$grade->ar_grade_name:$grade->en_grade_name}}</option>                                    
@@ -25,15 +25,15 @@
             </select>
         </div>
 
-        <div class="col-md-2 col-xs-12">
-            <select name="status_id" class="form-control select2" id="filter_status_id">
+        <div class="col-lg-2 col-md-5 mb-md-1">
+            <select name="status_id" class="form-control select2" style="width: 100%" id="filter_status_id">
                 <option value="">{{ trans('student::local.register_status_id') }}</option>
                 @foreach ($regStatus as $status)
                     <option value="{{$status->id}}">{{session('lang') == 'ar' ? $status->ar_name_status : $status->en_name_status}}</option>                                    
                 @endforeach
             </select>
         </div>  
-        <div class="col-md-4 col-xs-12">
+        <div class="col-lg-2 col-md-5 mb-md-1">
             <button onclick="filter()" formaction="#" class="btn btn-primary btn-sm"><i class="la la-search"></i></button>        
             <div class="btn-group ml-1">
                 <button type="button" class="btn btn-primary btn-min-width dropdown-toggle" data-toggle="dropdown"
