e84a43bf01e17dd92a11a3fe30147467b861fe8f
Abdoahmed111 <eng.abdelrahman.ahmed11@gmail.com>
2020-10-15 08:20:46 +0200

    fixing the loyout of the filter form in absence page

--- app/Modules/student/resources/views/students-affairs/absence/includes/_filter.blade.php
+++ app/Modules/student/resources/views/students-affairs/absence/includes/_filter.blade.php
@@ -2,8 +2,8 @@
     <div class="col-md-12">
         <form action="{{route('absences.create')}}" method="get" id="filterForm" >
             <div class="row mt-1">       
-                <div class="col-md-2">
-                    <select name="division_id" class="form-control" id="filter_division_id">
+                <div class="col-lg-2 col-md-4 mb-md-1 mb-lg-0">
+                    <select name="division_id" class="form-control" style="width: 100%" id="filter_division_id">
                         <option value="">{{ trans('student::local.divisions') }}</option>
                         @foreach ($divisions as $division)
                             <option value="{{$division->id}}">
@@ -11,8 +11,8 @@
                         @endforeach
                     </select>
                 </div>
-                <div class="col-md-2">
-                    <select name="grade_id" class="form-control" id="filter_grade_id">
+                <div class="col-lg-2 col-md-4 mb-md-1 mb-lg-0">
+                    <select name="grade_id" class="form-control" style="width: 100%" id="filter_grade_id">
                         <option value="">{{ trans('student::local.grades') }}</option>
                         @foreach ($grades as $grade)
                             <option value="{{$grade->id}}">
@@ -20,12 +20,12 @@
                         @endforeach
                     </select>
                 </div>
-                <div class="col-md-2">
-                    <select name="filter_classroom_id" class="form-control" id="filter_classroom_id" disabled>
+                <div class="col-lg-2 col-md-4 mb-md-1 mb-lg-0">
+                    <select name="filter_classroom_id" class="form-control" style="width: 100%" id="filter_classroom_id" disabled>
                         <option value="">{{ trans('student::local.classrooms') }}</option>                    
                       </select>
                 </div>    
-                <button onclick="filter()"  class="btn btn-primary mr-1"> {{ trans('student::local.search') }}</button>        
+                <button onclick="filter()"  class="btn btn-primary mr-1 ml-md-1 ml-lg-0"> {{ trans('student::local.search') }}</button>        
                 <button type="submit" class="btn btn-success mr-1"> {{ trans('student::local.add_absence') }}</button>                                                     
                 <button type="button" onclick="monthlyStatement()" class="btn btn-purple"> {{ trans('student::local.absence_statement') }}</button>                                                     
             </div>
