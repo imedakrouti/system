792fc5418e5d194636abe9370911ce99197fc4cb
Abdoahmed111 <eng.abdelrahman.ahmed11@gmail.com>
2020-10-15 08:43:13 +0200

    fixint the layout of the form of new-parent-request

--- app/Modules/student/resources/views/students-affairs/parent-requests/create.blade.php
+++ app/Modules/student/resources/views/students-affairs/parent-requests/create.blade.php
@@ -28,11 +28,10 @@
                 <div class="form-body">
                     <h4 class="form-section"> {{ $title }}</h4>
                     @include('layouts.backEnd.includes._msg')
-                    <div class="col-md-6">
-                        <div class="form-group row">
-                          <label class="col-md-3 label-control">{{ trans('student::local.student_name') }}</label>
-                          <div class="col-md-9">
-                            <select name="student_id[]" class="form-control select2" multiple required>
+                    <div class="col-lg-6 col-md-8">
+                        <div class="form-group">
+                          <label>{{ trans('student::local.student_name') }}</label> <br>
+                            <select name="student_id[]" class="form-control select2" style="width: 100%" multiple required>
                                 @foreach ($students as $student)
                                     <option {{old('student_id') == $student->id ? 'selected' :''}} value="{{$student->id}}">
                                         @if (session('lang') == 'ar')
@@ -42,36 +41,29 @@
                                     @endif
                                     </option>
                                 @endforeach
-                            </select>
+                            </select> <br>
                               <span class="red">{{ trans('student::local.requried') }}</span>
-                          </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="form-group row">
-                          <label class="col-md-3 label-control">{{ trans('student::local.date_request') }}</label>                         
-                          <div class="col-md-9">
+                    <div class="col-lg-6 col-md-8">
+                        <div class="form-group">
+                          <label>{{ trans('student::local.date_request') }}</label> <br>
                             <input type="date" class="form-control" required name="date_request" value="{{old('date_request')}}">
                             <span class="red">{{ trans('student::local.requried') }}</span>
-                          </div>
                         </div>
                     </div>                     
-                    <div class="col-md-6">
-                        <div class="form-group row">
-                          <label class="col-md-3 label-control">{{ trans('student::local.time_request') }}</label>
-                          <div class="col-md-9">                         
+                    <div class="col-lg-6 col-md-8">
+                        <div class="form-group">
+                          <label>{{ trans('student::local.time_request') }}</label> <br>                    
                             <input type="time" class="form-control" required name="time_request" value="{{old('time_request')}}">
                               <span class="red">{{ trans('student::local.requried') }}</span>
-                          </div>
                         </div>
                     </div>  
-                    <div class="col-md-6">
-                        <div class="form-group row">
-                          <label class="col-md-3 label-control">{{ trans('student::local.notes') }}</label>
-                          <div class="col-md-9">                         
+                    <div class="col-lg-6 col-md-8">
+                        <div class="form-group">
+                          <label>{{ trans('student::local.notes') }}</label> <br>                 
                               <textarea name="notes" class="form-control" required cols="30" rows="5">{{old('notes')}}</textarea>
                               <span class="red">{{ trans('student::local.requried') }}</span>
-                          </div>
                         </div>
                     </div>                                                        
                 </div>
