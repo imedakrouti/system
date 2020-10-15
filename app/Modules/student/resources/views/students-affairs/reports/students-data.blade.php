aea5f04e36f5c131f81e52ced130391b7cd54866
Abdoahmed111 <eng.abdelrahman.ahmed11@gmail.com>
2020-10-15 02:37:01 +0200

    fixing the fornt-end layout of students-data-report page

--- app/Modules/student/resources/views/students-affairs/reports/students-data.blade.php
+++ app/Modules/student/resources/views/students-affairs/reports/students-data.blade.php
@@ -27,48 +27,60 @@
                 <div class="col-md-12">
                     <h4>{{ trans('student::local.statistics_note') }} | <span class="blue">{{ trans('student::local.current_year') }} {{fullAcademicYear()}}</span></h4>
                     <form action="#" method="get" id="filterForm" target="_blank">
-                          <div class="row mt-1">
-                              <div class="col-md-2">
+                          <div class="row mt-1" >
+                              <div class="col-lg-3 mr-1 mb-md-1  col-md-5">
+                                <div class="row">
                                   <select name="year_id" class="form-control select2" id="filter_year_id">                                      
                                       @foreach ($years as $year)
                                           <option {{currentYear() == $year->id ? 'selected' : ''}} value="{{$year->id}}">{{$year->name}}</option>                                    
                                       @endforeach
-                                  </select>
+                                  </select>  
+                                </div>  
                               </div>        
-                              <div class="col-md-2">
+                              <div class="col-lg-3 mr-1 mb-md-1  col-md-5">
+                                <div class="row">
                                   <select name="division_id[]" class="form-control select2" multiple id="filter_division_id">                                      
                                       @foreach ($divisions as $division)
                                           <option {{session('division_id') == $division->id ? 'selected' : ''}} value="{{$division->id}}">
                                               {{session('lang') =='ar' ?$division->ar_division_name:$division->en_division_name}}</option>                                    
                                       @endforeach
                                   </select>
+                                </div>
                               </div> 
-                              <div class="col-md-2">
-                                <select name="grade_id" class="form-control select2" id="filter_grade_id">                
+                              <div class="col-lg-3 mr-1 mb-md-1  col-md-5">
+                                <div class="row">
+                                  <select name="grade_id" class="form-control select2" id="filter_grade_id">                
                                     @foreach ($grades as $grade)
                                         <option value="{{$grade->id}}">
                                             {{session('lang') =='ar' ?$grade->ar_grade_name:$grade->en_grade_name}}</option>                                    
                                     @endforeach
                                 </select>
+                                </div>
                               </div>                                
-                              <div class="col-md-2">
-                                <select name="room_id" class="form-control select2" id="filter_room_id" disabled>
+                              <div class="col-lg-3 mr-1 mb-md-1  col-md-5">
+                                <div class="row">
+                                  <select name="room_id" class="form-control select2" id="filter_room_id" disabled>
                                   <option value="">{{ trans('student::local.classrooms') }}</option>                    
                                 </select>
+                                </div>
                               </div> 
-                              <div class="col-md-2">
-                                <select name="lang_id" class="form-control select2" id="filter_lang_id">                
+                              <div class="col-lg-3 mr-1 mb-md-1  col-md-5">
+                                <div class="row">
+                                  <select name="lang_id" class="form-control select2" id="filter_lang_id">                
                                     @foreach ($languages as $language)
                                         <option value="{{$language->id}}">
                                             {{session('lang') =='ar' ?$language->ar_name_lang:$language->en_name_lang}}</option>                                    
                                     @endforeach
                                 </select>
+                                </div>
                               </div>
-                              <div class="col-md-2">
-                                <select name="religion_id" class="form-control select2" id="filter_religion_id">
+                              <div class="col-lg-3 mr-1 mb-md-1  col-md-5">
+                                <div class="row">
+                                  <select name="religion_id" class="form-control select2" id="filter_religion_id">
                                   <option value="muslim">{{ trans('student::local.muslim') }}</option>                    
                                   <option value="non_muslim">{{ trans('student::local.non_muslim') }}</option>                    
                                 </select>
+                                </div>
                               </div>                                                                                        
                           </div>
                     </form>
@@ -85,33 +97,19 @@
       <div class="card">
         <div class="card-content collapse show">
           <div class="card-body">  
-              <h4 class="purple">{{ trans('student::local.reports') }}</h4>           
-              <div class="row">
-                <div class="col-md-4">
-                    
+              <h4 class="purple ml-2">{{ trans('student::local.reports') }}</h4>
                     <div class="card-content collapse show">
                         <div class="card-body">
-                          <div class="list-group">
+                          <div class="list-group col-md-6">
                             <button type="button" onclick="printStatement()" class="list-group-item list-group-item-action">{{trans('student::local.students_statements') }}</button>
                             <button type="button" onclick="studentsContactsData()" class="list-group-item list-group-item-action">{{trans('student::local.student_contacts_data') }}</button>
-                            <button type="button" onclick="nameList()" class="list-group-item list-group-item-action">{{trans('student::local.name_list') }}</button>        
-                          </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    
-                    <div class="card-content collapse show">
-                        <div class="card-body">
-                          <div class="list-group">
+                            <button type="button" onclick="nameList()" class="list-group-item list-group-item-action">{{trans('student::local.name_list') }}</button> 
                             <button type="button" onclick="SecondlangData()" class="list-group-item list-group-item-action">{{trans('student::local.name_list_second_lang') }}</button>
                             <button type="button" onclick="religionData()" class="list-group-item list-group-item-action">{{trans('student::local.name_list_religion') }}</button>
-                            {{-- <button type="button" class="list-group-item list-group-item-action">{{trans('student::local.students_usernames') }}</button>         --}}
+                            {{-- <button type="button" class="list-group-item list-group-item-action">{{trans('student::local.students_usernames') }}</button>         --}}       
                           </div>
                         </div>
                     </div>
-                </div>
-              </div>
           </div>
         </div>
       </div>
