5b389c23ed36e9cbe0388d2e5c4ded96a2a2cbc5
amr <amr_dotnet@ymail.com>
2020-11-02 14:18:13 +0200

    fixed bug in index page for admin accounts

--- resources/views/admin/accounts/index.blade.php
+++ resources/views/admin/accounts/index.blade.php
@@ -59,8 +59,7 @@
 <script>
     $(function () {
         var myTable = $('#dynamic-table').DataTable({
-        @include('layouts.backEnd.includes.datatables._datatableConfig')
-            dom: 'Bfrtip',
+        @include('layouts.backEnd.includes.datatables._datatableConfig')            
             buttons: [
                 // new btn
                 {
